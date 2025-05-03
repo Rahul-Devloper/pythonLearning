f36ff2f9ad1adf499cc6b230eabe254d88ada53b
Rahul-Devloper <rahulramesh26@yahoo.com>
2025-05-03 19:01:35 +0100

    formatted string

--- string.py
+++ string.py
@@ -1,5 +1,10 @@
 course = "Python Programming"
-len(course)
 print(len(course))
 print(course[0])
 print(course[-1])
+print(course[0:3])
+doubleQuote = "Python \"Programming\""
+print(doubleQuote)
+first = "Python"
+last = "Programming"
+print(f"{first} {last}")
