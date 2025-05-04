ee87e5dfa5703da5aac6b3f5c149cbd847044132
Rahul-Devloper <rahulramesh26@yahoo.com>
2025-05-04 21:59:37 +0100

    string

--- string.py
+++ string.py
@@ -8,3 +8,9 @@ print(doubleQuote)
 first = "Python"
 last = "Programming"
 print(f"{first} {last}")
+print(course.upper())
+print(course.strip())  # remove space
+print(course.find('r'))  # find index of character
+# returns boolean. Checks if the character exists in string and return boolean.
+print('pro' in course)
+print('rahul' not in course)
