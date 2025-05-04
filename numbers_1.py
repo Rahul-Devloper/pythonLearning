6040297cdaf48979980a6d1dd10aef751d5571de
Rahul-Devloper <rahulramesh26@yahoo.com>
2025-05-04 22:56:52 +0100

    numbers

---
+++ numbers_1.py
@@ -0,0 +1,15 @@
+import math  # import math module
+# explore more on phython3 math module in google
+x = 1
+print(x)
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)  # float
+print(10 // 3)  # integer
+print(10 % 3)  # remainder
+print(10 ** 3)  # exponent
+
+print(math.floor(2.9))
+print(math.ceil(2.9))
+
