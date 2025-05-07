ee407eb0e43b59906d38135c1827cec3d21c462e
Rahul <rahulramesh26@yahoo.com>
2025-05-07 10:24:02 +0100

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -2,4 +2,8 @@ temperature = 20
 if temperature > 30: # always use colon at the end
     print("it's warm ")
     print("drink water")
+elif temperature > 20:
+    print("it's nice")
+else:
+    print("it's cold")
 print("done")
