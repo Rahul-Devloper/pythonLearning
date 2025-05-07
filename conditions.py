22e9086332442603bfdd04b6de975892d2a9cc72
Rahul-Devloper <rahulramesh26@yahoo.com>
2025-05-07 01:15:53 +0100

    conditions

---
+++ conditions.py
@@ -0,0 +1,5 @@
+temperature = 20
+if temperature > 30: # always use colon at the end
+    print("it's warm ")
+    print("drink water")
+print("done")
--- numbers.py
+++
@@ -1,9 +0,0 @@
-x = 1
-print(x)
-print(10 + 3)
-print(10 - 3)
-print(10 * 3)
-print(10 / 3) # float
-print(10 // 3) # integer
-print(10 % 3) # remainder
-print(10 ** 3) # exponent
